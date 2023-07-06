9fcd03709a6b0397e728912cf20b208f9c6fd694
pendevx <pendevx@gmail.com>
2023-07-07 01:22:44 +1200

    Create logic for scramble editor Scrambles generated are now within an official length getFontSize(): returns a new font size for every different scramble type handleGenerateScramble(): wrapper around setScramble() and setScrambleFont()

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,24 +4,79 @@ import Scrambo from "scrambo";
 import React from "react";
 
 const scrambleGenerator = new Scrambo();
+
 const generateScramble = () : string => {
+    const getRandomNumber = (min : number, max : number) : number => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    switch (scrambleGenerator.type()) {
+        case "333": {
+            scrambleGenerator.length(getRandomNumber(15, 21));
+        } break;
+
+        case "444": {
+            scrambleGenerator.length(getRandomNumber(42, 48));
+        } break;
+
+        case "555": {
+            scrambleGenerator.length(60);
+        } break;
+
+        case "666": {
+            scrambleGenerator.length(80);
+        } break;
+
+        case "777": {
+            scrambleGenerator.length(100);
+        } break;
+    }
+
     return scrambleGenerator.get(1)[0];
 }
 
+const getFontSize = (scrambleType : string) : string => {
+    switch (scrambleType) {
+        case "333": return "3rem";
+        case "444":
+        case "555": return "2.6rem";
+        case "666": return "2rem";
+        case "777": return "1.8rem";
+    }
+
+    return "";
+}
+
 function App() {
+    const [ scrambleFont, setScrambleFont ] = React.useState<string>("3rem");
     const [ scramble, setScramble ] = React.useState<string>(generateScramble());
+
+    const handleGenerateScramble = () => {
+        setScramble(generateScramble());
+        setScrambleFont(getFontSize(scrambleGenerator.type()));
+    }
+
+    const handleScrambleTypeChange = (type : string) => {
+        const typeName : any = type[0].repeat(3);
+        scrambleGenerator.type(typeName);
+
+        handleGenerateScramble();
+    }
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.header}>
-				<Scramble scramble={scramble} />
+				<Scramble scramble={scramble}
+                    switchScramble={() => handleGenerateScramble()}
+                    setScrambleType={type => handleScrambleTypeChange(type)}
+                    fontSize={scrambleFont} />
 			</div>
 			<div className={styles.layout}>
 				<div>
 					<InfoCard />
 				</div>
 				<div>
-					<Timer generateScramble={() => setScramble(generateScramble())} />
+					<Timer generateScramble={() => handleGenerateScramble()} />
 				</div>
 			</div>
 		</div>
