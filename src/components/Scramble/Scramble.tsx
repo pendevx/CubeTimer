2dc4f9a2dfd07577ed7f4de342172698a767488f
pendevx <pendevx@gmail.com>
2023-07-07 01:19:29 +1200

    Placed scramble editor component into scramble component

--- src/components/Scramble/Scramble.tsx
+++ src/components/Scramble/Scramble.tsx
@@ -1,13 +1,17 @@
 import styles from "./Scramble.module.css";
+import { ScrambleEditor } from "../ScrambleEditor";
+import { ScrambleEditorProps } from "../ScrambleEditor";
 
-interface ScrambleProps {
+interface ScrambleProps extends ScrambleEditorProps {
     scramble: string;
+    fontSize: string;
 }
 
 function Scramble(props: ScrambleProps) {
     return (
         <div className={styles.scrambleContainer}>
-            <p className={styles.scrambleText}>{props.scramble}</p>
+            <p className={styles.scrambleText} style={{fontSize:`${props.fontSize}`}}>{props.scramble}</p>
+            <ScrambleEditor switchScramble={props.switchScramble} setScrambleType={props.setScrambleType} />
         </div>
     );
 }
