fbcf5d67b02c0882b42a9255bb3c9505913ec36a
pendevx <pendevx@gmail.com>
2023-07-07 19:14:07 +1200

    Change style and position of scramble editor Scramble editor requires button to expand.

--- src/components/Scramble/Scramble.tsx
+++ src/components/Scramble/Scramble.tsx
@@ -1,19 +1,28 @@
 import styles from "./Scramble.module.css";
 import { ScrambleEditor } from "../ScrambleEditor";
 import { ScrambleEditorProps } from "../ScrambleEditor";
+import React from "react";
 
 interface ScrambleProps extends ScrambleEditorProps {
     scramble: string;
-    fontSize: string;
 }
 
 function Scramble(props: ScrambleProps) {
-    return (
-        <div className={styles.scrambleContainer}>
-            <p className={styles.scrambleText} style={{fontSize:`${props.fontSize}`}}>{props.scramble}</p>
-            <ScrambleEditor switchScramble={props.switchScramble} setScrambleType={props.setScrambleType} />
+    const [ showSettings, setShowSettings ] = React.useState<boolean>();
+
+    return (<>
+        <div className={styles.scrambleContainer} style={{backgroundColor:"#555555"}}>
+            <p className={styles.scrambleText}>{props.scramble}</p>
+            <button className={styles.toggleButton} onClick={() => setShowSettings(!showSettings)}>
+                <img src="/toggle-button.webp"></img>
+                <div className={styles.settingsDropdown}>
+                    { showSettings && 
+                        <ScrambleEditor switchScramble={props.switchScramble} setScrambleType={props.setScrambleType} />
+                    }
+                </div>
+            </button>
         </div>
-    );
+    </>);
 }
 
 export default Scramble;
