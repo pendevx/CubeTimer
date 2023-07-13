369b99b70db105e4b096c3666f71631df93d0034
pendevx <pendevx@gmail.com>
2023-07-13 23:40:25 +1200

    Add exports for components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,7 @@ export { App as default } from "./App";
 export { Timer } from "./Timer";
 export { InfoCard } from "./InfoCard";
 export { Scramble } from "./Scramble";
+export { ScrambleEditor } from "./ScrambleEditor";
+export { Accounts } from "./Accounts";
+export { TimeList } from "./TimeList";
+export { Settings } from "./Settings";
