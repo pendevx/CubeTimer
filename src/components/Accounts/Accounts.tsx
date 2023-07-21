922edc7330330b5b83fb4725c0362f265abcb530
pendevx <pendevx@gmail.com>
2023-07-21 18:50:23 +1200

    Update Account component Delete registration user interface (now lives in Account context as IRegisterDetails) Create different paths for authorization actions (login and register)
    Show AccountDetails if user is logged in, otherwise:
    Show Login if user wants to login, or
    Show Register if user wants to register

--- src/components/Accounts/Accounts.tsx
+++ src/components/Accounts/Accounts.tsx
@@ -1,26 +1,38 @@
 import styles from "./Accounts.module.css";
+import { useAccountContext } from "../../../contexts";
+import { AccountDetails, Login, Register } from "..";
+import React from "react";
 
-interface IRegistrationUser {
-    username: string;
-    password: string;
-    email: `${string}@${string}.${string}`;
-    name: string;
-}
+type LoginOrRegister = "login" | "register";
 
 function Accounts() {
-    const register = async () => {
+    const [ unauthorizedActions, setUnauthorizedActions ] = React.useState<LoginOrRegister>("login");
+    const context = useAccountContext();
 
+    const register = async () => {
+        // TODO
     }
 
     const login = async () => {
-        
+        context?.login({
+            username: "",
+            password: ""
+        });
     }
+
+    const result = context?.isLoggedIn ? <AccountDetails /> : (
+        unauthorizedActions === "login" ? 
+            <Login /> : 
+            <Register />
+    );
 
     return (
         <div className={styles.container}>
             <img src="/blank-profile.webp" />
+
+            { result }
         </div>
-    )
+    );
 }
 
 export default Accounts;
