3ec5c168f026c9090a4a1a1e177154541546a5f8
pendevx <pendevx@gmail.com>
2023-07-21 18:48:15 +1200

    Make changes to Account Context Hide setters for state Expose login, logout, and register functions These functions will set the values in the state on success

--- contexts/AccountContext.tsx
+++ contexts/AccountContext.tsx
@@ -1,3 +1,4 @@
+import { InitiateAuthCommandOutput } from "@aws-sdk/client-cognito-identity-provider";
 import React from "react";
 
 interface AccountContextProps {
@@ -5,12 +6,24 @@ interface AccountContextProps {
 }
 
 interface AccountContextType {
+    isLoggedIn: boolean;
     username: string;
-    setUsername: (val: string) => void;
     name: string;
-    setName: (val: string) => void;
     email: string;
-    setEmail: (val: string) => void;
+    login: (details: ILoginDetails) => void;
+    register: (details: IRegisterDetails) => void;
+}
+
+interface ILoginDetails {
+    username: string;
+    password: string;
+}
+
+interface IRegisterDetails {
+    username: string;
+    password: string;
+    email: `${string}@${string}.${string}`;
+    name: string;
 }
 
 const Context = React.createContext<AccountContextType | null>(null);
@@ -19,15 +32,39 @@ export function AccountContext({ children } : AccountContextProps) {
     const [ username, setUsername ] = React.useState<string>("");
     const [ name, setName ] = React.useState<string>("");
     const [ email, setEmail ] = React.useState<string>("");
+    const [ isLoggedIn, setIsLoggedIn ] = React.useState<boolean>(false);
+
+    const login = async ({ username, password } : ILoginDetails): Promise<void> => {
+        const res = await fetch(`https://api.cubetimer.pendevx.com/login?username=${username}&password=${password}`);
+
+        if (res.status < 200 || res.status > 299) {
+
+            return;
+        }
+        
+        const credentials : InitiateAuthCommandOutput = await res.json();
+
+        // set the value of the username, name, email, etc. below
+    }
+
+    const logout = async (): Promise<void> => {
+
+    }
+
+    const register = async ({ username, password, email, name } : IRegisterDetails) => {
+        console.log(username, password, email, name);
+        
+        // TODO
+    }
 
     return (
         <Context.Provider value={{
+            isLoggedIn,
             username,
-            setUsername,
             name,
-            setName,
             email,
-            setEmail
+            login,
+            register
         }}>
             {children}
         </Context.Provider>
